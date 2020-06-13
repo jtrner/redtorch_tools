32ec42b518c6d17d8dcb2d6edb14c9bd801a2256
ehsanm <ehsanm@iconcreativestudio.com>
2020-06-13 10:06:45 -0700

    2.31.0 -> ooo

--- src/rt_tools/package.py
+++ src/rt_tools/package.py
@@ -1,7 +1,7 @@
 import os
 
 
-__version__ = "2.30.3"
+__version__ = "2.31.0"
 __author__ = "Ehsan Hassani Moghaddam"
-__comment__ = "2.30.3 -> skincluster from deformLib fix"
+__comment__ = "2.31.0 -> ooo"
 os.environ["REDTORCH_TOOLS_VERSION"] = __version__
